a9f02dd45f3dff3acba3b40004c86fa82c396283
Azeez Adebayo <deeprog@zealcode.onmicrosoft.com>
2021-09-18 21:05:43 +0100

    added http interface and it implementation

---
+++ PlatformService/SyncDataService/Http/HttpCommandDataClient.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using PlatformService.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PlatformService.SyncDataService.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        public async Task SendPlatformToCommand(PlatformReadDto platformReadDto)
+        {
+            var httpContent = new StringContent(JsonSerializer.Serialize(platformReadDto), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync($"{_configuration["CommandService"]}/api/c/Platform", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Sync Post to Command Service was Ok");
+            }
+            else
+            {
+                Console.WriteLine("Sync Post to Command Service was not Ok");
+            }
+        }
+    }
+}
