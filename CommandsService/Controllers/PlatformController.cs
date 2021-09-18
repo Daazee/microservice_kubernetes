17300b11f3c09c22cb014964eb9a7d06200c4471
Azeez Adebayo <deeprog@zealcode.onmicrosoft.com>
2021-09-18 21:07:22 +0100

    Added command micro service project

---
+++ CommandsService/Controllers/PlatformController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        public PlatformController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInbound()
+        {
+            Console.WriteLine("---> Inbound Post # Command Service");
+            return Ok("Inbound test from platform controller");
+        }
+    }
+}
